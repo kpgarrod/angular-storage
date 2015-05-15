6578c3685cc637aa9ec16790f23e2422c4c37ae2
kpgarrod <kpgarrod@gmail.com>
2015-05-15 07:47:59 +0200

    Add webpack support

--- src/angularStorage/angularStorage.js
+++ src/angularStorage/angularStorage.js
@@ -1,3 +1,19 @@
+/**
+ * Add a module for use with commonjs(WebPack)
+ * Add to your webpack.config.js with:
+ * 
+ module.exports = {
+  resolve: {
+    alias: {
+      "angular-storage": 'angular-storage/dist/angular-storage'
+    }
+  }
+ }
+ */
+if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports ===
+  exports) {
+  module.exports = 'angular-storage';
+}
 // Create all modules and define dependencies to make sure they exist
 // and are loaded in the correct order to satisfy dependency injection
 // before all nested files are concatenated by Grunt
